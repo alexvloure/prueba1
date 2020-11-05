330bf3af0260c0a3e04d1b3cfebc0b56c565081d
alexvloure <email>
2020-11-05 15:12:26 +0100

    Google Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// Main files
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Components
 import { CorreoComponent } from './Components/correo/correo.component';
 import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
 import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
-import { AvisosComponent } from './Components/avisos/avisos.component';
-import { CorreosRecibidosComponent } from './Views/correos-recibidos/correos-recibidos.component';
+//import { AvisosComponent } from './Components/avisos/avisos.component';
 import { LoginComponent } from './Components/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
+
+// View and Menu
+import { MenuComponent } from './Menu/menu/menu.component';
+import { HomeComponent } from './Views/home/home.component';
+import { EnviarComponent } from './Views/enviar/enviar.component';
+import { VisualizarCorreoComponent } from './Views/visualizar-correo/visualizar-correo.component';
+import { CorreosRecibidosComponent } from './Views/correos-recibidos/correos-recibidos.component';
+
+// Modules
+import { MaterialModule } from './Modules/material/material.module';
+
+// External Libs
 import {
   GoogleApiModule,
   NgGapiClientConfig,
   NG_GAPI_CONFIG,
 } from "ng-gapi";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 let gapiClientConfig: NgGapiClientConfig = {
-  client_id: "395684135333-7cs6mldgfgr2q531bu51el0j4hdosv0s.apps.googleusercontent.com",
+  client_id: "858731323623-ahm3l5i8i4cie5bb5sqiqb1mfg8dsq3l.apps.googleusercontent.com",
   discoveryDocs: ["https://analyticsreporting.googleapis.com/$discovery/rest?version=v4"],
   ux_mode: "popup",
   redirect_uri: "http://localhost:4200/loged",
@@ -37,9 +52,13 @@ let gapiClientConfig: NgGapiClientConfig = {
     CorreoComponent,
     ListaCorreosComponent,
     NuevoCorreoComponent,
-    AvisosComponent,
+    // AvisosComponent,
     CorreosRecibidosComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    HomeComponent,
+    EnviarComponent,
+    VisualizarCorreoComponent
   ],
   imports: [
     BrowserModule,
@@ -51,6 +70,8 @@ let gapiClientConfig: NgGapiClientConfig = {
       provide: NG_GAPI_CONFIG,
       useValue: gapiClientConfig
     }),
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
